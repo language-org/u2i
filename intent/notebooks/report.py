71cc334fa7fc30a302580b9971d69bf007246037
steeve <steeve@stanford.edu>
2020-12-18 12:02:21 +0100

    Initialize report

---
+++ intent/notebooks/report.py
@@ -0,0 +1,18 @@
+# %%[markdown]
+#
+# # REPORT
+# author: Steeve LAQUITAINE
+#
+# **EXECUTIVE SUMMARY**
+#
+# 1. Sentence type classification
+# 1. Classification:
+#   * assess the amount of predictive lexical information in utterances
+#   * [Q]: what if low performance ?
+# 2. Intent detection:
+#   * assess whether an intent is expressed or not
+# 3. Intent parsing:
+#   * when an intent is expressed: extract it
+#       * syntactical parsing?
+#       * constituency parsing?
+
