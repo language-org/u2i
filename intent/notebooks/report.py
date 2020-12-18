c1df73b6c59228362e2b26d208fb5045bccbcda9
steeve <steeve@stanford.edu>
2020-12-18 12:21:02 +0100

    Edit report

--- intent/notebooks/report.py
+++ intent/notebooks/report.py
@@ -4,15 +4,28 @@
 # author: Steeve LAQUITAINE
 #
 # **EXECUTIVE SUMMARY**
+# * A
+# * B
 #
-# 1. Sentence type classification
+# **APPROACH**
+#
 # 1. Classification:
-#   * assess the amount of predictive lexical information in utterances
-#   * [Q]: what if low performance ?
+#   * Is there enough predictive lexical information in utterances to discriminate between intents
+#       * [Q]: Does low performance impair the quality of intent parsing ?
+#   * Characterise the predictive information:
+#       * the lexicon clearly differ b/w intents (clustering)
+#       * lexicon very stereotyped and redundant within an intent cluster
+#       * [Q]: Do overlapping lexicons or/and diverse lexicons impair the quality of intent parsing?
+#   * Does sentence type impair classification performance ?
+#       * why ?
 # 2. Intent detection:
 #   * assess whether an intent is expressed or not
 # 3. Intent parsing:
 #   * when an intent is expressed: extract it
 #       * syntactical parsing?
 #       * constituency parsing?
+#       * `intent-indicator`
+# 4. Compair w/ freer (less stereotyped/constrained) utterances:
+#   * Questions expressed in Forums?
 
+# %%
