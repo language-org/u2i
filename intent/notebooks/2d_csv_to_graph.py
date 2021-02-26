07796488c1e9e1ed4043dbcf7afdc6ee8a247f09
steeve <steeve@stanford.edu>
2021-02-26 15:18:00 +0100

    Vary graph layouts

--- intent/notebooks/2d_csv_to_graph.py
+++ intent/notebooks/2d_csv_to_graph.py
@@ -1,4 +1,3 @@
-
 # %% [markdown]
 ## CFG in .csv to graph
 
@@ -9,32 +8,115 @@ import networkx as nx
 import pandas as pd
 
 # %%
-proj_path= "/Users/steeve_laquitaine/desktop/CodeHub/intent/"
+proj_path = "/Users/steeve_laquitaine/desktop/CodeHub/intent/"
 os.chdir(proj_path)
 from intent.src.intent.nodes import graphs
 
 # %% [markdown]
 ## LOAD CFG DATA
 # %%
-cfg_path = proj_path + 'intent/data/02_intermediate/cfg_25_02_2021_18_16_42.xlsx'
+cfg_path = (
+    proj_path + "intent/data/02_intermediate/cfg_25_02_2021_18_16_42.xlsx"
+)
 data = pd.read_excel(cfg_path)
 # %% [markdown]
 ## PARAMETERS
 # %%from intent.src.intent.nodes import graphs
 isdirected = False
 isweighted = False
-size_window = 2 # bigrams
+size_window = 2  # bigrams
 # %% [markdown]
 ## EXTRACT VP CFG right side production
 # %%
-cfg = data['cfg']
-constituents = cfg.apply(lambda x: x.replace('VP ->', ''))
+cfg = data["cfg"]
+constituents = cfg.apply(lambda x: x.replace("VP ->", ""))
 
 # %% [markdown]
 ## TEXT TO GRAPH
 # %%
 # %%
-graph = graphs.from_text_to_graph(constituents, isdirected=isdirected, isweighted=isweighted, size_window=size_window)
+graph = graphs.from_text_to_graph(
+    constituents,
+    isdirected=isdirected,
+    isweighted=isweighted,
+    size_window=size_window,
+)
 # %% [markdown]
-# **Fig. text graph**
-nx.draw(graph, with_labels=True, font_weight='bold')
+# **Fig. text graph in different configurations**
+# %% [markdown]
+# **Spring**
+# %%
+nx.draw(
+    graph,
+    node_color="w",  # node facecolor
+    edgecolors="k",  # node boundary's color (3)
+    linewidths=1,
+    with_labels=True,
+    font_weight="bold",
+)
+# %% [markdown]
+# **Circular**
+# %%
+pos = nx.circular_layout(graph)
+nx.draw(
+    graph,
+    pos=pos,
+    node_color="w",  # node facecolor
+    edgecolors="k",  # node boundary's color (3)
+    linewidths=1,
+    with_labels=True,
+    font_weight="bold",
+)
+# %% [markdown]
+# **kamada_kawai**
+# %%
+pos = nx.kamada_kawai_layout(graph)
+nx.draw(
+    graph,
+    pos=pos,
+    node_color="w",  # node facecolor
+    edgecolors="k",  # node boundary's color (3)
+    linewidths=1,
+    with_labels=True,
+    font_weight="bold",
+)
+# %% [markdown]
+# **spring**
+# %%
+pos = nx.spring_layout(graph)
+nx.draw(
+    graph,
+    pos=pos,
+    node_color="w",  # node facecolor
+    edgecolors="k",  # node boundary's color (3)
+    linewidths=1,
+    with_labels=True,
+    font_weight="bold",
+)
+# %% [markdown]
+# **spectral**
+# %%
+pos = nx.spectral_layout(graph)
+nx.draw(
+    graph,
+    pos=pos,
+    node_color="w",  # node facecolor
+    edgecolors="k",  # node boundary's color (3)
+    linewidths=1,
+    with_labels=True,
+    font_weight="bold",
+)
+# %% [markdown]
+# **spectral**
+# %%
+pos = nx.spiral_layout(graph)
+nx.draw(
+    graph,
+    pos=pos,
+    node_color="w",  # node facecolor
+    edgecolors="k",  # node boundary's color (3)
+    linewidths=1,
+    with_labels=True,
+    font_weight="bold",
+)
+# %%
