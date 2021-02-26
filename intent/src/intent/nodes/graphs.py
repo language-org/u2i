9373149cede24f63ac7414b66e33f4b28a6d5c4a
steeve <steeve@stanford.edu>
2021-02-26 16:35:31 +0100

    Add function to convert cfg to constituents

--- intent/src/intent/nodes/graphs.py
+++ intent/src/intent/nodes/graphs.py
@@ -4,7 +4,9 @@ import networkx as nx
 import pandas as pd
 
 
-def get_graph_from_document(doc_str:str, isdirected:bool, isweighted:bool, size_window:int):
+def get_graph_from_document(
+    doc_str: str, isdirected: bool, isweighted: bool, size_window: int
+):
     """Convert string text to a networkx graph object
 
     Args:
@@ -19,25 +21,28 @@ def get_graph_from_document(doc_str:str, isdirected:bool, isweighted:bool, size_
 
     doc_array = doc_str.split()
     N = len(doc_array)
-    
+
     if isdirected:
         G = nx.DiGraph()
     else:
-        G=nx.Graph()
-        
+        G = nx.Graph()
+
     for j in range(N):
-        for i in range(max(j-size_window+1,0),j):
+        for i in range(max(j - size_window + 1, 0), j):
             if G.has_edge(doc_array[i], doc_array[j]):
                 if isweighted:
                     # we added this one before, just increase the weight by one
-                    G[doc_array[i]][doc_array[j]]['weight'] += 1
+                    G[doc_array[i]][doc_array[j]]["weight"] += 1
             else:
                 # new edge. add with weight=1
                 G.add_edge(doc_array[i], doc_array[j], weight=1)
 
     return G
 
-def get_gow(corpus:pd.Series, isdirected:bool, isweighted:bool, size_window:int):
+
+def get_gow(
+    corpus: pd.Series, isdirected: bool, isweighted: bool, size_window: int
+):
     """Convert pandas series of texts to a dictionary of networkx graph objects
 
     Args:
@@ -50,13 +55,21 @@ def get_gow(corpus:pd.Series, isdirected:bool, isweighted:bool, size_window:int)
         dict: [description]
     """
     dict_graph_of_words = dict()
-    
+
     for i in range(len(corpus)):
-         dict_graph_of_words[i] = get_graph_from_document(corpus[i],isdirected,isweighted, size_window)
-        
+        dict_graph_of_words[i] = get_graph_from_document(
+            corpus[i], isdirected, isweighted, size_window
+        )
+
     return dict_graph_of_words
 
-def from_sents_to_graphs(text:pd.Series, isdirected:bool=False, isweighted:bool=False, size_window:int=2)->dict:
+
+def from_sents_to_graphs(
+    text: pd.Series,
+    isdirected: bool = False,
+    isweighted: bool = False,
+    size_window: int = 2,
+) -> dict:
     """Convert text sentences to a dictionary of networkx graphs
     note: parallelizable 
 
@@ -73,9 +86,28 @@ def from_sents_to_graphs(text:pd.Series, isdirected:bool=False, isweighted:bool=
     graphs = get_gow(text, isdirected, isweighted, size_window)
 
     return graphs
-    
 
-def from_text_to_graph(text:pd.Series, isdirected:bool=False, isweighted:bool=False, size_window:int=2)->nx.classes.graph.Graph:
+
+def from_cfg_to_constituents(cfg: pd.Series) -> pd.Series:
+    """Convert a pandas series of string VP production rules ('VP -> V NP') to 
+    a series of string constituents ('V NP')
+
+    Args:
+        cfg (pd.Series): pandas series of string VP production rules ('VP -> V NP')
+
+    Returns:
+        pd.Series: a series of string constituents ('V NP')
+    """
+    constt = cfg.apply(lambda x: x.replace("VP ->", ""))
+    return constt
+
+
+def from_text_to_graph(
+    text: pd.Series,
+    isdirected: bool = False,
+    isweighted: bool = False,
+    size_window: int = 2,
+) -> nx.classes.graph.Graph:
     """Convert text sentences to a dictionary of networkx graphs
 
     Args:
@@ -87,13 +119,14 @@ def from_text_to_graph(text:pd.Series, isdirected:bool=False, isweighted:bool=Fa
     Returns:
         nx.classes.graph.Graph: dictionary of networkx graphs for each sentence
     """
-    
+
     # convert each sentence to a graph
-    # convert dictionary of graphs to list of graphs 
+    # convert dictionary of graphs to list of graphs
     # merge all graphs
-    graphs_by_sent = from_sents_to_graphs(text, isdirected, isweighted, size_window)
+    graphs_by_sent = from_sents_to_graphs(
+        text, isdirected, isweighted, size_window
+    )
     list_of_graphs = list(graphs_by_sent.values())
     graph = nx.compose_all(list_of_graphs)
 
     return graph
-    
