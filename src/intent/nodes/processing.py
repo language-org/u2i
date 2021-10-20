977fc3f92065a2d54667097eb4b55e1c24239f5a
steeve <steeve@stanford.edu>
2021-10-20 16:11:17 +0200

    Document processing

--- src/intent/nodes/processing.py
+++ src/intent/nodes/processing.py
@@ -102,36 +102,32 @@ class Processing:
         # parse constituents
         t_cfg = time()
         cfg = Cfg(corpus, self.params).do()
-        logger.info("Parsing constituents")
-        logger.info(f"N={len(cfg)} queries left")
-        logger.info(f"took {time()-t_cfg} secs")
+        self._log_cfg(t_cfg, cfg)
+
+        # inspect
         if self.inspect:
-            introsp["VP"] = None
-            introsp["cfg"] = None
-            introsp["VP"].loc[cfg["index"]] = cfg["VP"]
-            introsp["cfg"].loc[cfg["index"]] = cfg["cfg"]
+            introsp = self._inspect_cfg(introsp, cfg)
 
         # filter complexity
         t_cx = time()
         cfg_cx = preprocess.filter_n_sent_eq(
             cfg, self.NUM_SENT, verbose=True
         )
-        logger.info("Filtering complex queries")
-        logger.info(f"N={len(cfg_cx)} queries left")
-        logger.info(f"took {time()-t_cx} secs")
+        self._log_cpx(t_cx, cfg_cx)
+
+        # inspect
         if self.inspect:
-            introsp["filt_cplxity"] = None
-            introsp["filt_cplxity"].loc[cfg["index"]] = 0
-            introsp["filt_cplxity"].loc[cfg_cx["index"]] = 1
+            introsp = self._inspect_cplx(
+                introsp, cfg, cfg_cx
+            )
 
         # filter mood
         t_mood = time()
-        cfg_mood = preprocess.filter_in_only_mood(
+        cfg_mood = preprocess.filter_mood(
             cfg_cx, self.FILT_MOOD
         )
-        logger.info("Filtering moods")
-        logger.info(f"N={len(cfg_mood)} queries left")
-        logger.info(f"took {time()-t_mood} secs")
+        self._log_mood(t_mood, cfg_mood)
+
         # get constituents
         tag = parsing.from_cfg_to_constituents(
             cfg_mood["cfg"]
@@ -139,20 +135,16 @@ class Processing:
 
         # filter syntax
         t_sx = time()
-        similarity_matrix = Lcs().do(cfg_mood)
-        sim_ranked = similarity.rank_nearest_to_seed(
-            similarity_matrix, seed=self.SEED, verbose=True
+        similarity_matrix, filtered = self._filter_syntax(
+            cfg_mood, tag
         )
-        posting_list = retrieval.create_posting_list(tag)
-        ranked = similarity.print_ranked_VPs(
-            cfg_mood, posting_list, sim_ranked
-        )
-        filtered = similarity.filter_by_similarity(
-            ranked, self.INTENT_SCORE
-        )
-        logger.info("Filtering 'not-intent' syntax")
-        logger.info(f"N={len(filtered)} queries left")
-        logger.info(f"took {time()-t_sx} secs")
+        self._log_syntax(t_sx, filtered)
+
+        # inspect
+        if self.inspect:
+            introsp = self._inspect_syntax(
+                introsp, filtered
+            )
 
         # index
         raw_ix = cfg_mood["index"]
@@ -160,17 +152,23 @@ class Processing:
             filtered.index.values
         ]
 
-        # Infer and parametrize intents
-        # 1. Parse queries functionally
-        # 2. Extract named entities
-        # 3. Build intent (ROOT), intendeed (dobj) and entities (NER)
+        # Inference & slot filling
         intents = parsing.parse_intent(filtered)
-
-        # display (intent, intendeed)
-        cfg_mood.index = cfg_mood["index"]
         intents_df = pd.DataFrame(
             intents, index=filtered_raw_ix
         )
+
+        # inspect
+        if self.inspect:
+            introsp = self._inspect_intent(
+                introsp, intents_df
+            )
+        from ipdb import set_trace
+
+        set_trace()
+
+        # display (intent, intendeed)
+        cfg_mood.index = cfg_mood["index"]
         cfg_mood = cfg_mood.merge(
             intents_df, left_index=True, right_index=True,
         )[
@@ -212,3 +210,141 @@ class Processing:
         )
         similarity_matrix.to_csv(file_path)
         return (processed, intents)
+
+    def _filter_syntax(self, cfg_mood, tag):
+        """Process syntax
+
+        Args:
+            cfg_mood ([type]): [description]
+            tag ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        similarity_matrix = Lcs().do(cfg_mood)
+        sim_ranked = similarity.rank_nearest_to_seed(
+            similarity_matrix, seed=self.SEED, verbose=True
+        )
+        posting_list = retrieval.create_posting_list(tag)
+        ranked = similarity.print_ranked_VPs(
+            cfg_mood, posting_list, sim_ranked
+        )
+        filtered = similarity.filter_by_similarity(
+            ranked, self.INTENT_SCORE
+        )
+
+        return similarity_matrix, filtered
+
+    def _log_syntax(self, t_sx, filtered):
+        """Log syntax processing
+
+        Args:
+            t_sx ([type]): [description]
+            filtered ([type]): [description]
+        """
+        logger.info("Filtering 'not-intent' syntax")
+        logger.info(f"N={len(filtered)} queries left")
+        logger.info(f"took {time()-t_sx} secs")
+
+    def _log_mood(self, t_mood, cfg_mood):
+        """Log mood processing
+
+        Args:
+            t_mood ([type]): [description]
+            cfg_mood ([type]): [description]
+        """
+        logger.info("Filtering moods")
+        logger.info(f"N={len(cfg_mood)} queries left")
+        logger.info(f"took {time()-t_mood} secs")
+
+    def _log_cpx(self, t_cx, cfg_cx):
+        """Log complexity processing
+
+        Args:
+            t_cx ([type]): [description]
+            cfg_cx ([type]): [description]
+        """
+        logger.info("Filtering complex queries")
+        logger.info(f"N={len(cfg_cx)} queries left")
+        logger.info(f"took {time()-t_cx} secs")
+
+    def _log_cfg(self, t_cfg, cfg):
+        """Log context free grammar parsing
+
+        Args:
+            t_cfg ([type]): [description]
+            cfg ([type]): [description]
+        """
+        logger.info("Parsing constituents")
+        logger.info(f"N={len(cfg)} queries left")
+        logger.info(f"took {time()-t_cfg} secs")
+
+    def _inspect_intent(self, introsp, intents_df):
+        """Inspect processed intent
+
+        Args:
+            introsp ([type]): [description]
+            intents_df ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+
+        kept = introsp["filt_not_intent_sx"].notnull()
+        for col in intents_df.columns:
+            introsp[col] = None
+            introsp[col].loc[kept] = 0
+            introsp[col].loc[intents_df.index] = intents_df[
+                col
+            ]
+        return introsp
+
+    def _inspect_syntax(self, introsp, filtered):
+        """Inspect processed syntax 
+
+        Args:
+            introsp ([type]): [description]
+            filtered ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        introsp["filt_not_intent_sx"] = None
+        kept = introsp["filt_cplxity"].notnull()
+        introsp["filt_not_intent_sx"].loc[kept] = 0
+        introsp["filt_not_intent_sx"].loc[
+            filtered.index
+        ] = 1
+        return introsp
+
+    def _inspect_cplx(self, introsp, cfg, cfg_cx):
+        """Inspect processed complexity
+
+        Args:
+            introsp ([type]): [description]
+            cfg ([type]): [description]
+            cfg_cx ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        introsp["filt_cplxity"] = None
+        introsp["filt_cplxity"].loc[cfg["index"]] = 0
+        introsp["filt_cplxity"].loc[cfg_cx["index"]] = 1
+        return introsp
+
+    def _inspect_cfg(self, introsp, cfg):
+        """Inspect processed context free grammar
+
+        Args:
+            introsp ([type]): [description]
+            cfg ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        introsp["VP"] = None
+        introsp["cfg"] = None
+        introsp["VP"].loc[cfg["index"]] = cfg["VP"]
+        introsp["cfg"].loc[cfg["index"]] = cfg["cfg"]
+        return introsp
